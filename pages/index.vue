170699b6dca21c69e9968be982c5749eabf03757
Tom Pain <tom@myswft.com>
2020-11-20 15:05:03 +0000

    Very basic coming soon holding page

--- pages/index.vue
+++ pages/index.vue
@@ -2,8 +2,8 @@
   <v-row justify="center" align="center">
     <v-col cols="12" sm="8" md="6">
       <div class="text-center">
-        <h2>The Secret Drinks Club.</h2>
-        <h4>Hopefully coming soon</h4>
+        <h2>Secret Drinks Club</h2>
+        <h4>Coming soon</h4>
       </div>
     </v-col>
   </v-row>
@@ -15,3 +15,9 @@ export default {
 
 }
 </script>
+
+<style lang="scss" scoped>
+  h4 {
+    font-family: 'Poppins', sans-serif;
+  }
+</style>
