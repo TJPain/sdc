e72cba5d8dc7418728e80f3d2b421600138cf9c3
Tom Pain <hello@tompa.in>
2020-11-20 13:04:31 +0000

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <div class="text-center">
+        <h2>The Secret Drinks Club.</h2>
+        <h4>Coming soon</h4>
+      </div>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
---
+++ pages/welcome/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h2>The SDC boxes</h2>
+        <h2><nuxt-link to="/welcome/1">video 1</nuxt-link></h2>
+        <h2><nuxt-link to="/welcome/2">video 2</nuxt-link></h2>
+        <h2><nuxt-link to="/welcome/3">video 3</nuxt-link></h2>
+        <h2><nuxt-link to="/welcome/4">video 4</nuxt-link></h2>        
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
