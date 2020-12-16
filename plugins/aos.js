881a45f8419fee87ddd0535e9042500000c167bf
Tom Pain <tom@myswft.com>
2020-12-16 17:25:07 +0000

    aos ease added, disabled for mobile

--- plugins/aos.js
+++ plugins/aos.js
@@ -5,7 +5,9 @@ import 'aos/dist/aos.css'
 export default ({ app }) => {
   // eslint-disable-next-line
   app.AOS = new AOS.init({
-    disable: 'phone',
+    disable: window.innerWidth < 640,
     duration: 800,
+    easing: 'ease-in-out-cubic',
+    once: true
   })
 }
