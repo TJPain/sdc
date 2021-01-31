87c6e2894e415324d92787fd5a8450a776d721b6
Tom Pain <tom@myswft.com>
2021-01-31 14:49:43 +0000

    Optimonk plugin added

--- nuxt.config.js
+++ nuxt.config.js
@@ -31,7 +31,10 @@ export default {
   css: ['~layouts/global.scss'],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [{ src: '~/plugins/aos.js', mode: 'client' }],
+  plugins: [
+    { src: '~/plugins/aos.js', mode: 'client' },
+    { src: '~plugins/Optimonk.js', mode: 'client' },
+  ],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
